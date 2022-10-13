d3b90ad51420553078425ba76f6ca1a8ea04e8f6
sabiha <sabiha.uddin786@gmail.com>
2022-10-13 18:24:19 +0600

    current design complete

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_update/pages/detail_page.dart';
 import 'package:weather_update/pages/settings_page.dart';
 import 'package:weather_update/providers/weather_provider.dart';
 import 'package:weather_update/utils/constant.dart';
@@ -54,6 +55,9 @@ class _WeatherPageState extends State<WeatherPage> {
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 12),
                 children: [
                   _currentWeatherSection(),
+                  SizedBox(
+                    height: 10,
+                  ),
                   _forecasteWeatherSection()
                 ],
               )
@@ -80,23 +84,22 @@ class _WeatherPageState extends State<WeatherPage> {
     final current = provider.currentResponseModel;
     if (current != null) {
       return Container(
-            height: MediaQuery.of(context).size.height * .8,
-            margin: const EdgeInsets.all(2),
-            padding: const EdgeInsets.only(top: 50, left: 30, right: 30),
-            decoration: const BoxDecoration(
-                color: Color(0xff00A1FF),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.blue,
-                    blurRadius: 15.5,
-                    spreadRadius: 5,
-                  )
-                ]),
-        child: Column(
-            children: [
+        height: MediaQuery.of(context).size.height * .75,
+        margin: const EdgeInsets.all(2),
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+            color: Color(0xff00A1FF),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(60),
+                bottomRight: Radius.circular(60)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blue,
+                blurRadius: 15.5,
+                spreadRadius: 5,
+              )
+            ]),
+        child: Column(children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -139,156 +142,140 @@ class _WeatherPageState extends State<WeatherPage> {
           ),
           Image.asset(
             'images/thunder.png',
+            width: 150,
+            height: 200,
+            fit: BoxFit.cover,
           ),
-          Text(
-            '${current.main!.temp!.round()}$degree${provider.unitSymbol}',
-            style: txtTempBig80,
-          ),
-          Text(
-            'feels like ${current.main!.feelsLike}$degree${provider.unitSymbol}',
-            style: txtNormal16White54,
-          ),
-          const Divider(
-            color: Colors.white,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  const Icon(
-                    CupertinoIcons.wind,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    '${current.wind!.speed}m/s ',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    "Wind",
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(.6),
-                      fontSize: 16,
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  const Icon(
-                    Icons.water_drop,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    '${current.main!.humidity}% ',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    "Humidity",
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(.6),
-                      fontSize: 16,
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  const Icon(
-                    Icons.cloud,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    '${current.main!.pressure}Kmh ',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    "Pressure",
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(.6),
-                      fontSize: 16,
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-
-          /*Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network(
-                '$iconPrefix${current.weather![0].icon}$iconSufix',
-                fit: BoxFit.cover,
-              ),
-              Text(
-                '${current.main!.temp!.round()}$degree${provider.unitSymbol}',
-                style: txtTempBig80,
-              )
-            ],
-          ),
-          Text(
-            'feels like ${current.main!.feelsLike}$degree${provider.unitSymbol}',
-            style: txtNormal16White54,
-          ),
-          Text(
-            '${current.weather![0].main} ${current.weather![0].description}',
-            style: txtNormal16White54,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Wrap(children: [
-            Text(
-              'Humidity: ${current.main!.humidity}% ',
-              style: txtNormal16,
+          Container(
+            height: 180,
+            child: Stack(
+              children: [
+                Positioned(
+                    bottom: 0,
+                    top: 0,
+                    left: 0,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            '${current.main!.temp!.round()}$degree${provider.unitSymbol}',
+                            style: const TextStyle(
+                                height: 0.1,
+                                fontSize: 70,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'feels like ${current.main!.feelsLike}$degree${provider.unitSymbol}',
+                            style: txtNormal16White54,
+                          ),
+                          const Divider(
+                            color: Colors.white,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 10, right: 10, bottom: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    const Icon(
+                                      CupertinoIcons.wind,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      '${current.wind!.speed}m/s ',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 16,
+                                          color: Colors.white),
+                                    ),
+                                    Text(
+                                      "Wind",
+                                      style: TextStyle(
+                                        color: Colors.white.withOpacity(.6),
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    const Icon(
+                                      Icons.water_drop,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      '${current.main!.humidity}% ',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 16,
+                                          color: Colors.white),
+                                    ),
+                                    Text(
+                                      "Humidity",
+                                      style: TextStyle(
+                                        color: Colors.white.withOpacity(.6),
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    const Icon(
+                                      Icons.cloud,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      '${current.main!.pressure}Kmh ',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 16,
+                                          color: Colors.white),
+                                    ),
+                                    Text(
+                                      "Pressure",
+                                      style: TextStyle(
+                                        color: Colors.white.withOpacity(.6),
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Wrap(
+                            children: [
+                              Text(
+                                'Sunrise: ${getFormattedDateTime(current.sys!.sunrise!, 'hh:mm a')}',
+                                style: txtNormal16White54,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'Sunset: ${getFormattedDateTime(current.sys!.sunset!, 'hh:mm a')}',
+                                style: txtNormal16White54,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+              ],
             ),
-            Text(
-              'Pressure: ${current.main!.pressure}hPa ',
-              style: txtNormal16,
-            ),
-            Text(
-              'Visibility: ${current.visibility}meter ',
-              style: txtNormal16,
-            ),
-            Text(
-              'Wind Speed: ${current.wind!.speed}meter/sec ',
-              style: txtNormal16,
-            ),
-            Text(
-              'Degree: ${current.wind!.deg}$degree ',
-              style: txtNormal16,
-            ),
-          ]),*/
-          const SizedBox(
-            height: 20,
-          ),
-          Wrap(
-            children: [
-              Text(
-                'Sunrise: ${getFormattedDateTime(current.sys!.sunrise!, 'hh:mm a')}',
-                style: txtNormal16White54,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Sunset: ${getFormattedDateTime(current.sys!.sunset!, 'hh:mm a')}',
-                style: txtNormal16White54,
-              ),
-            ],
           )
         ]),
       );
@@ -296,11 +283,45 @@ class _WeatherPageState extends State<WeatherPage> {
     return const SizedBox();
   }
 
-   _forecasteWeatherSection() {
-    final forecast=provider.forecastResponseModel;
-    if(forecast!=null){
-
-
-    }
+  Widget _forecasteWeatherSection() {
+    final forecast = provider.forecastResponseModel;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          children: [
+            Text(
+              'Today',
+              style: TextStyle(
+                  color: Colors.white, fontSize: 16,),
+            ),
+          ],
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white
+            ),
+            onPressed:()=> Navigator.pushNamed(context, DetailPage.routeName),
+            
+            child: Row(
+              children: const [
+                Text('7 days',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        )),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.blue,
+                  size: 15,
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
